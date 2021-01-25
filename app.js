bcad28b231bc0707a262f743bb206422f311145f
David Fontenot <font21@gmail.com>
2021-01-25 17:27:30 -0600

    Moved the bulk of the functionality in to a single function. Much of the code works. Upload and file catch works. Able to slice first and last elements of each string. Error: Uncaught TypeError: sentenceArray[indexi][indexii].slice is not a function on line 40.

---
+++ app.js
@@ -0,0 +1,225 @@
+// Message if JavaScript is connected and running
+console.log('JavaScript connected.');
+
+
+// Page load timers : set
+const startTime = performance.now();
+
+
+/* ======================================
+	Functions
+===================================== */
+
+const staticFileInputFunction = () => {
+	const staticFileInput = document.querySelector('staticFileInput[type="file"]')
+
+	staticFileInput.addEventListener('submit', function (e) {
+		console.log(staticFileInput.files)
+		const reader = new FileReader()
+		reader.onload = function () {	
+			const lines = reader.result.split('\n').map(function (line) {
+				return line.split(',')
+			})
+			console.log(lines)
+		}
+		reader.readAsText(staticFileInput.files[0])
+	}, false)
+}
+
+
+// The Document Ready Event
+	$(document).ready(function(){
+
+	// Message if jQuery is connected and running
+		console.log('jQuery connected.');
+
+	/* ======================================
+		RADIO BUTTONs
+	=======================================*/
+	// to select static (single file upload) or Dynamic (multiple file upload).
+		$('input[type="radio"]').click(function(){
+			var inputValue = $(this).attr("value");
+			var targetBox = $("." + inputValue);
+			$(".box").not(targetBox).hide();
+			$(targetBox).show();
+		});
+
+/* Show More Buttons
+	https://www.youtube.com/watch?v=RVA4HoEE_q8&ab_channel=TheNetNinja
+*/
+	var dynamicContentText = document.getElementById("dynamicContentText");
+	var dynamicShowMoreButton = document.getElementById("dynamicShowMoreButton");
+
+	dynamicShowMoreButton.onclick = function(){
+		if(dynamicContentText.className == "open"){
+			// shrink the box
+			dynamicContentText.className = "";
+			dynamicShowMoreButton.innerHTML = "Show More";
+		} else {
+			// expand the box
+			dynamicContentText.className = "open";
+			dynamicShowMoreButton.innerHTML = "Show Less";
+		}
+	};
+
+
+
+
+
+	/* ======================================
+		Static
+		Static browse, upload single file, and process.
+	 ===================================== */
+	 staticFileInputFunction();
+		// const myStaticForm = document.getElementById("myStaticForm");
+		// const staticFile = document.getElementById("staticFile");
+
+		// myStaticForm.addEventListener("submit", e => {
+		// 	// Prevent page reload
+		// 		e.preventDefault();
+
+		// 	// const endpoint = "upload.php";
+		// 		const formData = new FormData();
+		// 		console.log(staticFile.files);
+		// 		formData.append("staticFile", staticFile.files[0]);	
+
+		// });
+
+		
+	});
+	
+
+
+/* -- Split-by-line code breaks DIVs displayed by radio button selector:
+
+	// File Reader and split by line
+	const reader = new.FileReader();
+	const lines = reader.result.split("\n");
+	console.log(lines[0])
+	console.log(lines[1])
+	console.log(lines[2])
+*/
+
+	/* ======================================
+		Dynamic
+	====================================== */
+	// Dynamic browse, upload multiple files, and process.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* =============================
+File upload code that did not work.
+ ============================== */
+
+
+/*
+const staticUploadForm = document.getElementById("staticUploadForm");
+const input = document.querySelector('input[type="file"]')
+
+			staticForm.addEventListener("staticSubmit", e => {
+				e.preventDefault();
+				console.log(input.staticFile)
+
+				formData.append("staticFile", staticFile.files[0]);
+
+				const reader = new FileReader()
+				reader.onload = function () {
+					console.log(reader.result)
+				}
+				reader.readAsText(input.files[0])
+				document.getElementById("fileDisplayer").innerHTML = reader.result;
+				document.getElementById("fileDisplayer").display = block;
+			}, false)
+	
+
+
+	const input = document.querySelector('input[type="file"]')
+
+	input.addEventListener('submit', function (e) {
+		console.log(input.files)
+		const reader = new FileReader()
+		reader.onload = function () {
+			console.log(reader.result)
+		}
+		reader.readAsText(input.files[0])
+		document.getElementById("fileDisplayer").innerHTML = reader.result;
+		document.getElementById("fileDisplayer").display = block;
+	}, false)
+*/
+
+
+
+/*
+function previewFile() {
+	const preview = document.querySelector('img');
+	const file = document.querySelector('input[type=file]').files[0];
+	const reader = new FileReader();
+  
+	reader.addEventListener("load", function () {
+	  // convert image file to base64 string
+	  preview.src = reader.result;
+	}, false);
+  
+	if (file) {
+	  reader.readAsDataURL(file);
+	}
+  }
+
+function updateSize() {
+    let nBytes = 0,
+        oFiles = this.files,
+        nFiles = oFiles.length;
+    for (let nFileId = 0; nFileId < nFiles; nFileId++) {
+      nBytes += oFiles[nFileId].size;
+    }
+    let sOutput = nBytes + " bytes";
+    // optional code for multiples approximation
+    const aMultiples = ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+    for (nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
+      sOutput = nApprox.toFixed(3) + " " + aMultiples[nMultiple] + " (" + nBytes + " bytes)";
+    }
+    // end of optional code
+    document.getElementById("fileNum").innerHTML = nFiles;
+    document.getElementById("fileSize").innerHTML = sOutput;
+  }
+
+  document.getElementById("uploadInput").addEventListener("change", updateSize, false);
+*/
+
+
+
+
+
+
+/* Page load timers : print times
+https://gist.github.com/prof3ssorSt3v3/5f976023977a299d80a03367f2352ef2
+load VS DOMContentLoaded Event
+https://www.youtube.com/watch?v=8rc0zaTn2ew&ab_channel=SteveGriffith
+*/
+	window.addEventListener('load', (ev)=>{
+		console.log(ev.type);
+		let endTime = performance.now();
+		let diff = endTime - startTime;
+		console.log(ev.type + " is " + diff + " milliseconds");
+	});
+
+	document.addEventListener('DOMContentLoaded', (ev)=>{
+		console.log(ev.type);
+		let endTime = performance.now();
+		let diff = endTime - startTime;
+		console.log(ev.type + " is " + diff + " milliseconds");
+	});
---
+++ static.js
@@ -0,0 +1,281 @@
+/* ==========================================================
+#															#
+#	Define Functions										#
+#															#
+========================================================== */
+
+// define global variables by using var at creation
+	var mergedPayload = "Nothing added to the mergedPayload varibale.";
+	var sentenceArray = [];
+
+function sentenceParserFunction (inputBlock) {
+
+	// Reassign inputBlock to sentenceArray for preservation
+		sentenceArray = inputBlock;
+
+	// Foor loop to itterate through the array
+		for (let indexi = 0; indexi < sentenceArray.length; indexi++) {
+
+			sentenceArray[indexi].forEach(function(elementii, indexii, arrayii) {
+				
+	// console.log(sentenceArray)
+		console.log('sentenceArray[0] = ' + sentenceArray[indexi]);
+		console.log('sentenceArray[0][0] = ' + sentenceArray[indexi][indexii]);
+
+		console.log('sentenceArray[1] = ' + sentenceArray[1]);
+		console.log('sentenceArray[1][0] = ' + sentenceArray[1][0]);
+
+		// For each object in the Array: slice out the talker and sentence type. Then, stick those back in.
+		// sentenceArray[1].forEach(function(sentencei){
+
+			
+		// DeBug: Print sentencei position
+			console.log('Debug: forEach(sentencei)')
+
+			console.log(`Debug: sentenceArray[${indexi}] = ${sentenceArray[indexi]}`);
+			console.log(`Debug: sentenceArray[${indexi}][${indexii}] = ` + sentenceArray[indexi][indexii]);
+
+		// Separate the first value of sentenceArray into talkerId and sentenceId values
+			
+			let talkerId = sentenceArray[indexi][indexii].slice(1, 3);
+			console.log("talkerId: " + talkerId);
+			let sentenceType = sentenceArray[indexi][indexii].slice(3);
+			console.log("sentenceType: " + sentenceType);
+
+		// and stick them at the front of sentenceArray
+			sentenceArray[indexi].unshift({ talkerId }, { sentenceType });
+		
+		// Remove the last element of the array.
+			let lastValue = sentenceArray[indexi].pop();
+		
+		// Print the values to the command line for debug.
+			let lastElementLength = indexii.length;
+			console.log('lastValue: ' + lastValue);
+			console.log('lastElement Length: ' + lastElementLength);
+		
+		// Separate last two elements of the array as second to last and checksum values:
+			let lastElementLengthIsh = lastElementLength - 2;
+			let penultimateElementStop = lastElementLength - 3;
+			let penultimateID = lastValue.slice(0, penultimateElementStop);
+			let checksum = lastValue.slice(4);
+		
+		// Print the values to the command line for debug.
+			console.log('penultimateID: ' + penultimateID);
+			console.log('checksum: ' + checksum);
+		
+		// Then, push those two values at the end of sentenceArray.
+			sentenceArray.push({ penultimateID }, { checksum });
+		
+		
+		// Define Keys to be paired, later
+			const cfgKeys = [
+				"talkerId",
+				"sentenceType",
+				"B",
+				"C",
+				"D",
+				"E",
+				"F",
+				"G",
+				"H",
+				"I",
+				"J",
+				"K",
+				"L",
+				"M",
+				"N",
+				"O",
+				"P",
+				"Q",
+				"R",
+				"S",
+				"T",
+				"U",
+				"V",
+				"W",
+				"X",
+				"checksum"
+			];
+		
+		
+			const ggaKeys = [
+				"talkerId",
+				"sentenceType",
+				"utcFromDevice",
+				"latitude",
+				"latitudeDirection",
+				"longitude",
+				"longitudeDirection",
+				"gpsQuality",
+				"satellitesInUse",
+				"dilutionOfPrecision",
+				"antennaAltitude",
+				"antennaAltitudeUnit",
+				"geoidalSeparation",
+				"geoidalSeparationUnit",
+				"ageOfData",
+				"checksum",
+			];
+		
+		
+			const gstKeys = [
+				"talkerId",
+				"sentenceType",
+				"TcOfAssociatedGgaFix",
+				"StandardDeviationMetersOfSemiMajor",
+				"StandardDeviationMetersOfSemiMinor",
+				"OrientationOfSemiMajorAxisOfErrorEllipse",
+				"StandardDeviationMetersOfLatitudeError",
+				"StandardDeviationMetersOfLongitudeError",
+				"StandardDeviationMetersOfAltitudeError",
+				"checksum",
+			];
+		
+		
+			const vtgKeys = [
+				"talkerId",
+				"sentenceType",
+				"CourseOverGroundDegreesTrue",
+				"T",
+				"CourseOverGroundDegreesMagnetic",
+				"M",
+				"SpeedOverGroundKnots",
+				"N",
+				"SpeedOverGroundKmh",
+				"K",
+				"FaaModeIndicator",
+				"checksum",
+			];
+		
+		
+			const zdaKeys = [
+				"talkerId",
+				"sentenceType",
+				"utc",
+				"Day",
+				"Month",
+				"Year",
+				"LocalZone",
+				"LocalZoneMinutes",
+				"checksum",
+			];
+		
+		
+		// If statement depending on the second element in the array (sentenceType), prepare for sending to InfluxDB.
+		if ( sentenceArray[1] == "$CFG" ) {
+		
+			// Do CFG things
+				console.log('Doing CFG things.');
+		
+			// Merge Key-Value pairs as defined above
+			// https://stackoverflow.com/a/50985915/13849868
+				sentenceArray = cfgKeys.reduce(
+					(arr, key, index) => [...arr, { [key]: values[index] }],
+					[]
+				); //  returns [{talkerId: '$GN'},{sentenceType: 'GGA'},{utcFromDevice: '...]
+		
+		}
+		else if ( sentenceArray[1] == "$GGA" ) {
+			
+			// Do GGA things
+				console.log('Doing GGA things.');
+		
+			// Merge Key-Value pairs as defined above
+			// https://stackoverflow.com/a/50985915/13849868
+				sentenceArray = ggaKeys.reduce(
+					(arr, key, index) => [...arr, { [key]: values[index] }],
+					[]
+				); //  returns [{talkerId: '$GN'},{sentenceType: 'GGA'},{utcFromDevice: '...]
+		
+				if(sentenceArray.latitudeDirection === 'S') {
+					sentenceArray.latitude = '-'+ sentenceArray.latitude
+				}
+				
+				if(sentenceArray.longitudeDirection === 'W') {
+					sentenceArray.longitude = '-'+ sentenceArray.longitude
+				}
+		
+		}
+		else if ( sentenceArray[1] == "$GST" ) {
+			
+			// Do GST things
+				console.log('Doing GST things.');
+		
+			// Merge Key-Value pairs as defined above
+			// https://stackoverflow.com/a/50985915/13849868
+				sentenceArray = gstKeys.reduce(
+					(arr, key, index) => [...arr, { [key]: values[index] }],
+					[]
+				); //  returns [{talkerId: '$GN'},{sentenceType: 'GST'},{TcOfAssociatedGgaFix: '...]
+		
+		}
+		else if ( sentenceArray[1] == "$VTG" ) {
+			
+			// Do VTG things
+				console.log('Doing VTG things.');
+		
+			// Merge Key-Value pairs as defined above
+			// https://stackoverflow.com/a/50985915/13849868
+				sentenceArray = vtgKeys.reduce(
+					(arr, key, index) => [...arr, { [key]: values[index] }],
+					[]
+				); //  returns [{talkerId: '$GN'},{sentenceType: 'VTG'},{CourseOverGroundDegreesTrue: '...]
+		
+		}
+		else if ( sentenceArray[1] == "$ZDA" ) {
+			
+			// Do ZDA things
+				console.log('Doing ZDA things.');
+		
+			// Merge Key-Value pairs as defined above
+			// https://stackoverflow.com/a/50985915/13849868
+				sentenceArray = zdaKeys.reduce(
+					(arr, key, index) => [...arr, { [key]: values[index] }],
+					[]
+				); //  returns [{talkerId: '$GN'},{sentenceType: 'ZDA'},{utc: '...]
+			
+		}
+		else {
+			console.log('Sentence type failed.');
+		}
+
+		// closes the for each sentenceii in Array loop.
+		}
+		)};
+	// closes the for each sentencei in Array loop.
+		};
+
+// Collapse the array and prepare to send out by assigning value to msg.payload
+	mergedPayload = sentenceArray.reduce((r,c) => ({...r,...c}), {})
+
+
+/* ==========================================================
+#															#
+#	End Defined functions and Begin Page Load				#
+#															#
+========================================================== */
+
+
+const input = document.querySelector('input[type="file"]')
+
+input.addEventListener('change', function (e) {
+	console.log(input.files)
+	const reader = new FileReader()
+	reader.onload = function () {
+		let lines = reader.result.split('\n').map(function (line) {
+			return line.split(',')
+		})
+		sentenceParserFunction(lines);
+
+		// Print everything or something
+			console.log(mergedPayload);
+			// msg.payload = mergedPayload
+
+			let div = document.createElement('div');			
+	
+			div.innerHTML = mergedPayload;
+			document.body.append(div);
+
+	}
+	reader.readAsText(input.files[0])
+}, false)
